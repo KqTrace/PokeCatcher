4794a4cff8bbe421dcd64cc06541676a94e8b91d
Kaique <souzakaique2@gmail.com>
2019-10-05 20:09:16 -0300

    Primeira classe

---
+++ usuario.php
@@ -0,0 +1,21 @@
+<?php
+class usuario{
+    //Atributos
+    private $username;
+    private $email;
+    private $senha;
+    private $experiencia;
+    private $dinheiro;
+    private $pokeCapturados;
+    private $pokemonTime;
+    private $badges;
+    private $pokebola;
+    private $consumiveis;
+
+
+    public function __construct(){
+        this->username = "Nikita";
+        this->email = "Nikita@Testing.com.br";
+    }
+
+}
